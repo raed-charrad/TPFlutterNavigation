940eb4c93b0cd4c50fd05ea431fca17c58b66d9c
raed-charrad <raed.charrad91@gmail.com>
2022-11-03 22:35:55 +0100

    add second page

--- lib/main.dart
+++ lib/main.dart
@@ -48,6 +48,23 @@ class FirstWidget extends StatelessWidget {
     ));
   }
 }
+class SecondWidget extends StatelessWidget {
+  int p1;
+  SecondWidget({this.p1 = 0});
+  @override
+  Widget build(BuildContext context) {
+    return (MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text("Second Widget")
+              ),
+            body: Center(
+              child: DogInformation(dogList[p1]),
+              
+            ))));
+  }
+}
 Widget DogList(Dog dog) {
   return Card (
               margin: EdgeInsets.all(10),
@@ -72,7 +89,7 @@ Widget DogList(Dog dog) {
                           Text(dog.name, style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold)),
                           Container(
                             decoration: BoxDecoration(
-                              color : dog.gender=='Male' ? Color.fromARGB(152, 33, 149, 243) : Color.fromARGB(255, 255, 192, 243),
+                              color : dog.gender=='Male' ? Color.fromARGB(255, 207, 231, 238) : Color.fromARGB(255, 255, 192, 243),
                               borderRadius: BorderRadius.circular(10),
                             ),
                             padding: EdgeInsets.all(5),
@@ -105,29 +122,19 @@ Widget DogList(Dog dog) {
             ),
             );
 }
-class SecondWidget extends StatelessWidget {
-  int p1;
-  SecondWidget({this.p1 = 0});
-  @override
-  Widget build(BuildContext context) {
-    return (MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text("Second Widget")
-              ),
-            body: Center(
-              child: 
-              Column(
+Widget DogInformation(Dog dog){
+  return Container(
+    padding: EdgeInsets.all(5),
+    child: Column(
                 children: [
-                  Image.asset("drawable/${dogList[p1].image}"),
+                  Image.asset("drawable/${dog.image}", width: double.infinity, height: 250, fit: BoxFit.cover),
                   Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(16, 0, 16, 0),
+                      padding: EdgeInsetsDirectional.fromSTEB(16, 10, 16, 0),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Text(
-                            dogList[p1].name,
+                            dog.name,
                             style: const TextStyle(
                               fontFamily: 'Lexend Deca',
                               color: Color(0xFF151B1E),
@@ -138,21 +145,156 @@ class SecondWidget extends StatelessWidget {
                           Container(
                             padding: const EdgeInsets.all(5),
                             decoration: BoxDecoration(
-                              color: dogList[p1].gender == "Male"
+                              color: dog.gender == "Male"
                                   ? Color.fromARGB(255, 207, 231, 238)
                                   : Color.fromARGB(255, 255, 192, 203),
                               borderRadius: BorderRadius.circular(15),
                             ),
-                            child: Text(dogList[p1].gender,
+                            child: Text(dog.gender,
                                 style: TextStyle(
-                                    color: dogList[p1].gender == "Male"
+                                    color: dog.gender == "Male"
                                         ? Colors.blue
                                         : Colors.pink)),
                           ), 
                         ],
                       )),
+                  Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(16, 10, 16, 0),
+                      child: Row(
+                        children: [
+                          Icon(Icons.location_on, size: 15, color: Colors.red),
+                          Text(" ${dog.distance}",
+                              style: const TextStyle(
+                                fontFamily: 'Lexend Deca',
+                                color: Color(0xFF151B1E),
+                                fontSize: 12,
+                                fontWeight: FontWeight.w500,
+                              )),
+                        ],
+                      )),
+                  Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(16, 10, 16, 0),
+                      // spaceBetween
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "12 min ago",
+                            style: const TextStyle(
+                              fontFamily: 'Lexend Deca',
+                              color: Color(0xFF151B1E),
+                              fontSize: 12,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          Text(
+                            "${dog.age} yrs | Playfull",
+                            style: const TextStyle(
+                              fontFamily: 'Lexend Deca',
+                              color: Color(0xFF151B1E),
+                              fontSize: 12,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      )),
+                  Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(16, 32, 16, 8),
+                      child: Text(
+                        "About ${dog.name}",
+                        textAlign: TextAlign.left,
+                        style: const TextStyle(
+                          fontFamily: 'Lexend Deca',
+                          color: Color(0xFF151B1E),
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )),
+                  Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(16, 10, 16, 0),
+                      child: Text(
+                        dog.description,
+                        textAlign: TextAlign.left,
+                        style: const TextStyle(
+                          fontFamily: 'Lexend Deca',
+                          color: Color(0xFF151B1E),
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      )),
+                  Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(16, 32, 16, 8),
+                      child: Text(
+                        "Quick Infos",
+                        textAlign: TextAlign.left,
+                        style: const TextStyle(
+                          fontFamily: 'Lexend Deca',
+                          color: Color(0xFF151B1E),
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )),
+                  Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(16, 10, 16, 0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                          children: [
+                            Text(" ${dog.age} yrs",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.black54,
+                                    fontWeight: FontWeight.w500)),
+                          ],
+                        ),
+                        Row(children: [
+                          Text(" ${dog.color}",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.black54,
+                                    fontWeight: FontWeight.w500)),
+                        ],),
+                        Row(children: [
+                          Text(" ${dog.weight} kg",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.black54,
+                                    fontWeight: FontWeight.w500)),
+                        ],)
+                        ],
+                      )
+                  ),
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(16, 10, 16, 0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                          children: [
+                            Text("age",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.black54,
+                                    fontWeight: FontWeight.w500)),
+                          ],),
+                          Row(children: [
+                            Text("color",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.black54,
+                                    fontWeight: FontWeight.w500)),
+                          ],),
+                          Row(children: [
+                            Text("weight",
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.black54,
+                                    fontWeight: FontWeight.w500)),
+                          ],)
+                ],),
+                  )
                 ],
               ),
-            ))));
-  }
+  );
 }
